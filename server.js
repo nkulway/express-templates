43d61dc47f7af4e844dc3061dc5644c5d4c35ce4
Nick Kulway <nkulway@gmail.com>
2021-10-05 20:36:13 -0400

    created simple template

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const http = require('http')
-const express = require('express')
+const http = require("http");
+const express = require("express");
+const es6Renderer = require("express-es6-template-engine");
 
-const hostname = '127.0.0.1'
-const port = 3000
+const hostname = "127.0.0.1";
+const port = 3000;
 
 const app = express();
-
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+// configure the template engine
+app.engine("html", es6Renderer) // register the template engine
+app.set("views", "views") // where to look for templates
+app.set('view engine', 'html') // which template engine should express use
 
-server.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}`)
+app.get("/", (req, res) => {
+  res.render('home') // views/home.html
 });
 
+server.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}`);
+});
